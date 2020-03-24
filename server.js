abc335cdc6e9a55a7dec0d2e55980f019805ebe6
Oren Magene <orenmagen100@gmail.com>
2020-03-24 16:30:12 -0600

    another trial

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
