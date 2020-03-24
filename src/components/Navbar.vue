21ba7c9ffcab2b9535af15327d927c4f72b7aac1
Oren Magene <orenmagen100@gmail.com>
2020-03-24 13:14:54 -0600

    adjust package json

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,11 +17,11 @@
           <b-navbar-nav class="ml-auto">
             <b-nav-form>
               <b-form-input
-                size="sm"
+                size="md"
                 class="mr-sm-2"
                 placeholder="Search"
               ></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              <b-button size="md" class="my-2 my-sm-0" type="submit"
                 >Search</b-button
               >
             </b-nav-form>
