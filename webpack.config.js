262e7c285f8878050ef244b61f1ca4d67b850b18
Oren Magene <orenmagen100@gmail.com>
2020-03-28 13:18:01 -0600

    improve webapcl

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,29 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     publicPath: "/",
-    filename: "[name].bundle.js"
+    filename: "[name].[contenthash].js"
   },
   optimization: {
+    runtimeChunk: "single",
     splitChunks: {
-      chunks: "all"
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace("@", "")}`;
+          }
+        }
+      }
     }
   },
   devServer: {
